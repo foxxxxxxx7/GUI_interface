53155cfd119b029f387a5a6fc4ce5141837413c8
foxxxxxxx7 <rob.fox7@hotmail.com>
2024-07-25 21:05:03 +0100

    COmplete Basic Window

--- main.py
+++ main.py
@@ -2,7 +2,10 @@ from tkinter import *
 
 window = Tk() # instatiate an instance of a window
 window.geometry("690x690")
-window.title("Fox's firt window")
-window.icon
+window.title("Fox's first window")
+
+icon = PhotoImage(file='Fox Logo PNG.png')
+window.iconphoto(True, icon)
+window.config(background="#FF0827")
 
 window.mainloop() # shows window on screen, listens for events
