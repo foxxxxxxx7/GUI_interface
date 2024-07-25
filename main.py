8c052f353df32fe4b4f7e09db7c8c9213d3e8a64
foxxxxxxx7 <rob.fox7@hotmail.com>
2024-07-25 20:58:29 +0100

    Add Image

--- main.py
+++ main.py
@@ -1,16 +1,8 @@
-# This is a sample Python script.
+from tkinter import *
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+window = Tk() # instatiate an instance of a window
+window.geometry("690x690")
+window.title("Fox's firt window")
+window.icon
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+window.mainloop() # shows window on screen, listens for events
