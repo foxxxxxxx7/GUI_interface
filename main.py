04c4cb95269844fd414515d94592df3208a765fd
foxxxxxxx7 <rob.fox7@hotmail.com>
2024-07-26 19:41:59 +0100

    Add Button Functionality

--- main.py
+++ main.py
@@ -1,11 +1,46 @@
 from tkinter import *
 
-window = Tk() # instatiate an instance of a window
-window.geometry("690x690")
+window = Tk()  # instatiate an instance of a window
 window.title("Fox's first window")
 
 icon = PhotoImage(file='Fox Logo PNG.png')
+icon2 = PhotoImage(file= 'Fox Logo tiny.png')
+photo = PhotoImage(file='Profile Circular Small.png')
 window.iconphoto(True, icon)
-window.config(background="#FF0827")
+window.config(background="#FFFFFF")
 
-window.mainloop() # shows window on screen, listens for events
+count = 0
+def click():
+    global count
+    count+=1
+    print("The button has been clicked", count, "Times!")
+
+button = Button (window,
+                 text="Click Here!",
+                 command=click,
+                 font=("Helvetica",20),
+                 fg="white",
+                 bg="black",
+                 activeforeground="white",
+                 activebackground="black",
+                 state=ACTIVE,
+                 image=icon2,
+                 compound='bottom')
+
+button.pack()
+
+# label = Label(window,
+#               text="Fox's Label",
+#               font=('Gothic', 40, 'italic'),
+#               fg='white',
+#               bg='black',
+#               relief=FLAT,
+#               bd=15,
+#               padx=30,
+#               pady=30,
+#               image=photo,
+#               compound='bottom')
+# label.pack()
+# label.place(x=300, y=325)
+
+window.mainloop()  # shows window on screen, listens for events
