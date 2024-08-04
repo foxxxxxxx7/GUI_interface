5d906133ced474728c87c68f83b3629f8e6ce3ca
foxxxxxxx7 <rob.fox7@hotmail.com>
2024-08-04 09:18:55 +0100

    Add move image on window

--- main.py
+++ main.py
@@ -170,32 +170,60 @@ from tkinter import *
 # def doSomething(event):
 #     print("Click coordinates are as follows: " + str(event.x)+","+str(event.y))
 
-def drag_start(event):
-    widget = event.widget
-    widget.startX = event.x
-    widget.startY = event.y
+# def drag_start(event):
+#     widget = event.widget
+#     widget.startX = event.x
+#     widget.startY = event.y
+#
+# def drag_motion(event):
+#     widget = event.widget
+#     x = widget.winfo_x() - widget.startX + event.x
+#     y = widget.winfo_y() - widget.startY + event.y
+#     widget.place(x=x,y=y)
 
-def drag_motion(event):
-    widget = event.widget
-    x = widget.winfo_x() - widget.startX + event.x
-    y = widget.winfo_y() - widget.startY + event.y
-    widget.place(x=x,y=y)
+def move_up(event):
+    label.place(x=label.winfo_x(), y=label.winfo_y()-10)
+
+def move_down(event):
+    label.place(x=label.winfo_x(), y=label.winfo_y()+10)
+
+def move_left(event):
+    label.place(x=label.winfo_x()-10, y=label.winfo_y())
+
+def move_right(event):
+    label.place(x=label.winfo_x()+10, y=label.winfo_y())
+
 
 window = Tk()  # instatiate an instance of a window
 window.title("Fox's first window")
-# window.geometry("500x500")
+window.geometry("500x500")
 
-label = Label(window, bg="Light blue", width=10, height=5)
+window.bind("<w>",move_up)
+window.bind("<s>",move_down)
+window.bind("<a>",move_left)
+window.bind("<d>",move_right)
+window.bind("<Up>",move_up)
+window.bind("<Down>",move_down)
+window.bind("<Left>",move_left)
+window.bind("<Right>",move_right)
+
+
+myImage = PhotoImage(file="fire.png")
+label = Label(window,
+              image=myImage)
 label.place(x=0,y=0)
 
-label2 = Label(window, bg="Light green", width=10, height=5)
-label2.place(x=100,y=100)
-
-label.bind("<Button-1>",drag_start)
-label.bind("<B1-Motion>",drag_motion)
-
-label2.bind("<Button-1>",drag_start)
-label2.bind("<B1-Motion>",drag_motion)
+# label = Label(window, bg="Light blue", width=10, height=5)
+# label.place(x=0,y=0)
+#
+# label2 = Label(window, bg="Light green", width=10, height=5)
+# label2.place(x=100,y=100)
+#
+# label.bind("<Button-1>",drag_start)
+# label.bind("<B1-Motion>",drag_motion)
+#
+# label2.bind("<Button-1>",drag_start)
+# label2.bind("<B1-Motion>",drag_motion)
 
 # window.bind("<Button-1>", doSomething)  # left click
 # window.bind("<Button-2>", doSomething)  # scroll wheel click
